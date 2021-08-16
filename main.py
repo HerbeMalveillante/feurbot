25ec286927152b39c79f33740a0208803812304b
herbemalveillante <herbemalveillante@gmail.com>
2021-08-16 18:31:29 +0200

    adding code

---
+++ main.py
@@ -0,0 +1,52 @@
+import discord
+
+
+from discord.ext import commands
+
+intents = discord.Intents.all()
+
+try :
+    with open("token.txt") as text_file:
+        token = text_file.read()
+except FileNotFoundError:
+    print("Error : a file named 'token.txt' containing your bot token has not been found. Please create one for the bot to run correctly.")
+    exit()
+
+bot = commands.Bot(command_prefix="f!", description="a very simple yet necessary bot for any server", intents = intents)
+bot.remove_command("help")
+
+@bot.event
+async def on_ready():
+    print("------------")
+    print(f"logged in successfully as {bot.user.name} (id : {bot.user.id})")
+    await bot.change_presence(activity=discord.Game(name="coiffeur ahah"))
+    print("LOADING COMPLETE")
+    print("------------")
+
+@bot.event
+async def on_message(message):
+    if message.content.lower().endswith("quoi"):
+        await message.channel.send("feur")
+        print(f"ahah on vient d'avoir {message.author}")
+    elif message.content.lower().endswith("oui"):
+        await message.channel.send("stiti")
+        print(f"ahah on vient d'avoir {message.author}")
+    elif message.content.lower().endswith("non"):
+        await message.channel.send("bril")
+        print(f"ahah on vient d'avoir {message.author}")
+    elif message.content.lower().endswith("ouais"):
+        await message.channel.send("stern")
+        print(f"ahah on vient d'avoir {message.author}")
+
+
+    if message.content == "f!servers":
+        await message.channel.send(f"Je suis actuellement sur {len(bot.guilds)} serveurs. Tapez `f!serverlist` pour en afficher la liste.")
+    
+    elif message.content == "f!serverlist":
+        serverlist = '\n'.join([guild.name + ' - (' + str(len(guild.members)) + ' membres)' for guild in bot.guilds])
+        await message.channel.send(f"Liste des serveurs : {serverlist}")
+
+bot.run(token, bot=True, reconnect=True)
+print("CONNEXION TERMINATED")
+
+
